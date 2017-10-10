18cf3d6f497e91f773032ea10dc4f1cab96aab5e
AxelCourt <a.court-ext@lefebvre-sarrut.eu>
2017-10-10 16:44:00 +0200

    Adding initial files

---
+++ load/src/main/java/fr/askjadev/xml/marklogic/load/filesystem/SimpleFileSystemLoader.java
@@ -0,0 +1,86 @@
+package fr.askjadev.xml.marklogic.load.filesystem;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import com.marklogic.client.io.*;
+import com.marklogic.client.datamovement.DataMovementManager;
+import com.marklogic.client.datamovement.WriteBatcher;
+import com.marklogic.client.DatabaseClientFactory;
+import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.DatabaseClientFactory.BasicAuthContext;
+
+public class SimpleFileSystemLoader {
+
+    // MarkLogic Server connection information
+    private final String HOST;
+    private final int PORT;
+    private final String USER;
+    private final String PASSWORD;
+    private final String DATA_DIR;
+    private final String DEST_DIR;
+    private final String DEST_COLL;
+    private DatabaseClient client;
+    
+    public SimpleFileSystemLoader(String host, String port, String user, String password, String dataDir, String destDir, String destColl) {
+        super();
+        this.HOST = host;
+        this.PORT = Integer.parseInt(port);
+        this.USER = user;
+        this.PASSWORD = password;
+        this.DATA_DIR = dataDir;
+        this.DEST_DIR = destDir;
+        this.DEST_COLL = destColl;
+    }
+
+    // Connection
+    private void connect() {
+        this.client = DatabaseClientFactory.newClient(HOST, PORT, new BasicAuthContext(USER, PASSWORD));
+    }
+
+    // Loading files into the database asynchronously
+    private void importDocs() {
+        // Create and configure the job
+        DataMovementManager dmm = client.newDataMovementManager();
+        WriteBatcher batcher = dmm.newWriteBatcher();
+        batcher.withBatchSize(100)
+                .withThreadCount(3)
+                .onBatchSuccess(batch -> {
+                    System.out.println(
+                            batch.getTimestamp().getTime()
+                            + " documents written: "
+                            + batch.getJobWritesSoFar());
+                })
+                .onBatchFailure((batch, throwable) -> {
+                    throwable.printStackTrace();
+                });
+
+        // Start the job and feed input to the batcher
+        dmm.startJob(batcher);
+        try {
+            Files.walk(Paths.get(DATA_DIR))
+                    .filter(Files::isRegularFile)
+                    .forEach(p -> {
+                        String uri = DEST_DIR + p.getFileName().toString();
+                        FileHandle handle
+                                = new FileHandle().with(p.toFile());
+                        batcher.add(uri, handle);
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Start any partial batches waiting for more input, then wait
+        // for all batches to complete. This call will block.
+        batcher.flushAndWait();
+        dmm.stopJob(batcher);
+    }
+
+    // Main
+    public static void main(String[] args) {
+        SimpleFileSystemLoader fsLoader = new SimpleFileSystemLoader(args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
+        fsLoader.connect();
+        fsLoader.importDocs();
+    }
+
+}
---
+++ query/src/main/java/fr/askjadev/xml/marklogic/query/SimpleElementTextQuery.java
@@ -0,0 +1,71 @@
+package fr.askjadev.xml.marklogic.query;
+
+import com.marklogic.client.datamovement.DataMovementManager;
+import com.marklogic.client.DatabaseClientFactory;
+import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.DatabaseClientFactory.BasicAuthContext;
+import com.marklogic.client.datamovement.QueryBatcher;
+import com.marklogic.client.query.QueryManager;
+import com.marklogic.client.query.StringQueryDefinition;
+
+public class SimpleElementTextQuery {
+    
+    // MarkLogic Server connection information
+    private final String HOST;
+    private final int PORT;
+    private final String USER;
+    private final String PASSWORD;
+    private final String DOC_DIR;
+    private final String SEARCH_QUERY;
+    private DatabaseClient client;
+    
+    public SimpleElementTextQuery(String host, String port, String user, String password, String docDir, String searchQuery) {
+        super();
+        this.HOST = host;
+        this.PORT = Integer.parseInt(port);
+        this.USER = user;
+        this.PASSWORD = password;
+        this.DOC_DIR = docDir;
+        this.SEARCH_QUERY = searchQuery;
+    }
+    
+    // Connection
+    private void connect() {
+        this.client = DatabaseClientFactory.newClient(HOST, PORT, new BasicAuthContext(USER, PASSWORD));
+    }
+    
+    // Query
+    private void query() {
+        // Construct the query
+        QueryManager queryMgr = client.newQueryManager();
+        StringQueryDefinition query = queryMgr.newStringDefinition();
+        query.setDirectory(DOC_DIR);
+        query.setCriteria(SEARCH_QUERY);
+        // Create and configure the batcher
+        DataMovementManager dmm = client.newDataMovementManager();
+        QueryBatcher batcher = dmm.newQueryBatcher(query);
+        batcher
+            .onUrisReady( batch -> {
+                for (String uri : batch.getItems()) {
+                    System.out.println(uri);
+                }
+            })
+            .onQueryFailure( exception -> exception.printStackTrace() );
+        
+        // Start the job
+        dmm.startJob(batcher);
+        
+        // Wait for the job to complete, and then stop it.
+        batcher.awaitCompletion();
+        dmm.stopJob(batcher);
+    
+    }
+
+    // Main
+    public static void main(String[] args) {
+        SimpleElementTextQuery eltQuery = new SimpleElementTextQuery(args[0], args[1], args[2], args[3], args[4], args[5]);
+        eltQuery.connect();
+        eltQuery.query();
+    }
+    
+}
